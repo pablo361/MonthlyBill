514f1bbc3fb7590111a03b2b15cec5f7fa38aa19
Pablo Pizarro <pablopizarro7@me.com>
2022-02-05 15:31:37 -0300

    Initial Commit

---
+++ MonthlyBill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MonthlyBill
+//
+//  Created by Pablo Pizarro on 05/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MonthlyBill/MonthlyBillApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonthlyBillApp.swift
+//  MonthlyBill
+//
+//  Created by Pablo Pizarro on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MonthlyBillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
